b2eb8ff5ebc57ccaa29717be709437c687722699
caoyucong <whosmeya@gmail.com>
2019-06-17 11:04:36 +0800

    first commit

---
+++ utils/fetch.ts
@@ -0,0 +1,42 @@
+import { message } from 'antd';
+
+interface MyInit extends RequestInit {
+  returnResponse?: boolean;  // 返回原数据 默认false  
+  returnBoolean?: boolean;   // 返回布尔   默认false
+  errorToast?: boolean;      // 错误toast  默认true
+}
+
+const defaultInit = {
+  returnResponse: false,
+  returnBoolean: false,
+  errorToast: true,
+}
+
+export default (input: RequestInfo, init: MyInit = defaultInit) => {
+  const { returnResponse, returnBoolean, errorToast } = init;
+  return fetch(input, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Duliday-Token': localStorage.DL_WO_token,
+    },
+    ...init,
+  })
+    .then(res => {
+      if (returnResponse) return res;  // returnResponse
+      return res.json();
+    })
+    .then(res => {
+      if (returnResponse) return res;  // returnResponse
+      if (res.code === 0) {
+        if (returnBoolean) return true;  // returnBoolean
+        return res.data;
+      } else {
+        if (errorToast) message.error(res.message);  // errorToast
+        return false;
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      return false;
+    })
+}
