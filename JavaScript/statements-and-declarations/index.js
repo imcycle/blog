f6bee99615ff10036d374182f72b74c2f7f5efb7
whosmeya <whosmeya@gmail.com>
2019-10-21 15:19:38 +0800

    update

--- JavaScript/statements-and-declarations/index.js
+++ JavaScript/statements-and-declarations/index.js
@@ -1,4 +1,5 @@
 /**
+ * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements
  * JavaScript 语句和声明
  * JavaScript 应用程序是由许多语法正确的语句组成的。
  * 单个语句可以跨多行。如果每个语句用分号隔开，那么多个语句可以在一行中出现。本页的内容并不是一个关键字，而是一组关键字。
@@ -9,7 +10,22 @@
 /**
  * 控制流程
  */
-
+// Block
+// 一个块语句可以用来管理零个或多个语句。该区块是由一对大括号分隔。
+// break
+// 终止当前的循环，switch 或 label 语句，使程序跳到下一个语句执行。
+// continue
+// 终止执行当前或标签循环的语句，直接执行下一个迭代循环。
+// Empty
+// 空语句用来表示没有语句的情况，尽管 JavaScript 语法期望有语句提供。
+// if...else
+// 如果指定的条件是 true ，则执行相匹配的一个语句，若为 false，则执行另一个语句。
+// switch
+// 计算表达式，将子句于表达式的值做匹配，执行与该值相关联的语句。
+// throw
+// 抛出一个用户定义的异常。
+// try...catch
+// 标记一个语句块，并指定一个应该抛出异常的反馈。
 
 
 
@@ -17,14 +33,28 @@
 /**
  * 声明
  */
-
+// var
+// 声明一个变量，可同时将其初始化为一个值。
+// let
+// 声明一个块级本地变量，可同时将其初始化为一个值。
+// const
+// 声明一个只读的命名常量。
 
 
 
 /**
  * 函数和类
  */
-
+// function
+// 声明一个指定参数的函数。
+// function*
+// 生成器函数使迭代器更容易使用。
+// async function
+// 使用指定的参数声明一个异步函数。
+// return
+// 指定函数的返回值。
+// class
+// 声明一个类。
 
 
 
@@ -32,9 +62,39 @@
 /**
  * 迭代器
  */
-
+// do...while
+// 创建一个循环来执行语句，直到该语句条件表达式的值为 false。先执行语句，再执行条件表达式，该语句至少会执行一次。
+// for
+// 创建一个由3个可选的表达式组成的循环，该循环用括号包裹，分号分割，并在循环体中执行语句。
+// for each...in
+// 通过指定的变量迭代对象所有属性的值。针对每个唯一的属性，会执行指定的语句块。
+// for...in
+// 无序遍历对象的可枚举属性。语句针对每个唯一的属性。
+// for...of
+// 遍历可迭代的对象（包括 数组、类数组对象、迭代器和生成器) ，对每个不同属性的属性，调用一个自定义的有执行语句的迭代钩子。
+// for await...of
+// Iterates over async iterable objects, array - like objects, iterators and generators, invoking a custom iteration hook with statements to be executed for the value of each distinct property.
+// while
+// 创建一个循环语句，循环会一直持续到该语句条件表达式的值为false。先执行条件表达式，然后执行语句。
 
 
 /**
  * 其他
  */
+// debugger
+// 调用可用的调试功能。如果没有调试功能可用，该语句不生效。
+// export
+// 用来导出函数，以便这些函数能够被导入到外部模块或其他脚本中。
+// import
+// 用来引入外部的模块或另一个script中导出的函数。
+// import.meta
+// A meta - property exposing context - specific metadata to a JavaScript module.
+// label
+// 带标识的语句，与 break 或 continue 语句一起使用。
+// with 
+// 拓展一个语句的作用域。
+
+
+
+
+
