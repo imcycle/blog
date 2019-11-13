84092870f1634c2b3b15b8064e42595102384449
whosmeya <whosmeya@gmail.com>
2019-11-13 19:06:19 +0800

    update

--- interview/bind.js
+++ interview/bind.js
@@ -1,30 +1,47 @@
-
-Function.prototype.bind = Function.prototype.bind || function bind(thisArg) {
+Function.prototype.mybind = function (thisArg) {
   if (typeof this !== 'function') {
-    throw new TypeError(this + ' must be a function');
+    throw TypeError("Bind must be called on a function");
   }
-  var self = this;
-  var args = [].slice.call(arguments, 1);
-  var bound = function () {
-    var boundArgs = [].slice.call(arguments);
-    var finalArgs = args.concat(boundArgs);
-    if (this instanceof bound) {
-      if (self.prototype) {
-        function Empty() { }
-        Empty.prototype = self.prototype;
-        bound.prototype = new Empty();
-      }
-      var result = self.apply(this, finalArgs);
-      var isObject = typeof result === 'object' && result !== null;
-      var isFunction = typeof result === 'function';
-      if (isObject || isFunction) {
-        return result;
-      }
-      return this;
-    }
-    else {
-      return self.apply(thisArg, finalArgs);
-    }
-  };
+  // 拿到参数，为了传给调用者
+  const args = Array.prototype.slice.call(arguments, 1),
+    // 保存 this
+    self = this,
+    // 构建一个干净的函数，用于保存原函数的原型
+    nop = function () { },
+    // 绑定的函数
+    bound = function () {
+      // this instanceof nop, 判断是否使用 new 来调用 bound
+      // 如果是 new 来调用的话，this的指向就是其实例，
+      // 如果不是 new 调用的话，就改变 this 指向到指定的对象 o
+      return self.apply(
+        this instanceof nop ? this : thisArg,
+        args.concat(Array.prototype.slice.call(arguments))
+      );
+    };
+
+  // 箭头函数没有 prototype，箭头函数this永远指向它所在的作用域
+  if (this.prototype) {
+    nop.prototype = this.prototype;
+  }
+  // 修改绑定函数的原型指向
+  bound.prototype = new nop();
+
   return bound;
 }
+
+
+
+
+const bar = function () {
+  console.log(this.name, arguments);
+};
+
+bar.prototype.name = 'bar';
+
+const foo = {
+  name: 'foo'
+};
+
+const bound = bar.mybind(foo, 22, 33, 44);
+new bound(); // bar, [22, 33, 44]
+bound(); // foo, [22, 33, 44]
