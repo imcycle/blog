409bac178df84b82f3fdeccc1522da692db55636
whosmeya <whosmeya@gmail.com>
2019-11-28 19:56:08 +0800

    docs

---
+++ docs/Web/JavaScript/Element.js
@@ -0,0 +1,16 @@
+// https://developer.mozilla.org/zh-CN/docs/Web/API/Element
+/**
+ * Element
+ * 是一个通用性非常强的基类，所有 Document 对象下的对象都继承自它。
+ * 这个接口描述了所有相同种类的元素所普遍具有的方法和属性。
+ */
+
+
+
+
+
+
+
+
+Element.scrollTop
+// 返回类型为：Number ，表示该元素纵向滚动条距离
---
+++ docs/Web/JavaScript/MouseEvent.js
@@ -0,0 +1,29 @@
+MouseEvent.offsetX
+// 事件对象与目标节点的内填充边（padding edge）在 X 轴方向上的偏移量。
+MouseEvent.offsetY
+// 事件对象与目标节点的内填充边（padding edge）在 Y 轴方向上的偏移量。
+
+MouseEvent.clientX
+// 应用客户端区域的水平坐标
+MouseEvent.clientY
+// 鼠标指针在点击元素（DOM）中的Y坐标。
+
+MouseEvent.screenX
+// 鼠标指针相对于全局（屏幕）的X坐标；
+MouseEvent.screenY
+// 鼠标指针相对于全局（屏幕）的Y坐标；
+
+
+MouseEvent.movementX
+MouseEvent.movementY
+// 鼠标指针相对于最后mousemove事件位置的Y坐标。
+
+
+MouseEvent.pageX
+// 鼠标指针相对于整个文档的X坐标；
+MouseEvent.pageY
+// 鼠标指针相对于整个文档的Y坐标；
+
+
+
+
